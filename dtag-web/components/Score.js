0303a1d11ce786d196bac0f18cf23650b61d4ed0
Palash A <palash.agarwal.96@gmail.com>
2022-07-19 08:02:37 +0530

    score display

--- dtag-web/components/Score.js
+++ dtag-web/components/Score.js
@@ -1,8 +1,45 @@
+import { useState, useEffect } from 'react';
+import {
+  ButtonGroup,
+  Button
+} from '@chakra-ui/react';
+
+import { useGameProvider } from '../context/GameContext';
+import { useWeb3Provider } from '../context/Web3Context';
+
 export default function Score({
 
 }) {
 
+  const [ lifetime, setLifetime ] = useState(0);
+  const [ scoreDisplay, setScoreDisplay ] = useState(0);
+  const [ scoreText, setScoreText ] = useState('lifetime');
+  
+  const { score } = useGameProvider();
+
+  const getLifetimeScore = async () => {
+    // TODO get from contract
+  }
+
+  const changeScore = () => {
+    if(scoreText === 'lifetime') {
+      setScoreText('current');
+      setScoreDisplay(score);
+    } else {
+      setScoreText('lifetime');
+      setScoreDisplay(lifetime);
+    }
+  }
+
   return (
-    <></>
+    <ButtonGroup
+      p={2} size='sm' isAttached 
+      colorScheme='highlights' 
+      variant='outline'
+      fontFamily='Comfortaa' 
+    >
+      <Button disabled>{scoreText} score</Button>
+      <Button onClick={changeScore}>{scoreDisplay}</Button>
+    </ButtonGroup>
   );
 }
