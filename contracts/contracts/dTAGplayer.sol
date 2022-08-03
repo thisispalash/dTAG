83eab823905cf4df60e46420ee29ac2826f98b23
Palash A <palash.agarwal.96@gmail.com>
2022-08-03 14:01:21 +0530

    some thoughts

--- contracts/contracts/dTAGplayer.sol
+++ contracts/contracts/dTAGplayer.sol
@@ -4,3 +4,13 @@ pragma solidity ^0.8.9;
 contract dTAGplayer {
 
 }
+/**
+ *
+ * Player contract is a rollup contract
+ * This enables a chain in browser
+ * user deposits some funds in this contract
+ * rollups decided by user (time)
+ * when rolled up, the txns to other addr happen on L2
+ * (which are finally rolled up to L1)
+ * 
+ */
